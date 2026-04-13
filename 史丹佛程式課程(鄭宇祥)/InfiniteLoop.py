cc0dd071d3327eb9e006c39a34a3aeaea97aaad1
nelson1998056 <nelson1998056@gmail.com>
2026-04-13 17:15:35 +0800

    Update InfiniteLoop.py

--- 史丹佛程式課程(鄭宇祥)/InfiniteLoop.py
+++ 史丹佛程式課程(鄭宇祥)/InfiniteLoop.py
@@ -22,7 +22,8 @@ def main():
     while front_is_clear():
         turn_left()
 """
-如果當karel前面沒有障礙物時，則左轉。但由於每次左轉後的前方都沒有障礙物，導致Karel一直無限迴轉，形成無限loop。
+如果當karel前面沒有障礙物時，則左轉。
+但由於每次左轉後的前方都沒有障礙物，導致Karel一直無限迴轉，形成無限loop。
 """
 
 # ----- DO NOT MODIFY CODE BELOW THIS LINE ----- #
